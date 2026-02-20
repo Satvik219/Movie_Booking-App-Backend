29e515ee2e586e5247afee189cbe36969f52678c
Satvik219 <satvik21092005@gmail.com>
2026-02-20 19:53:06 +0530

    backend done

--- src/main/java/org/satvik/moviebookingsystembackend/controller/TheatreController.java
+++ src/main/java/org/satvik/moviebookingsystembackend/controller/TheatreController.java
@@ -1,9 +1,7 @@
 package org.satvik.moviebookingsystembackend.controller;
 
 
-import org.satvik.moviebookingsystembackend.entity.Screen;
 import org.satvik.moviebookingsystembackend.entity.Theatre;
-import org.satvik.moviebookingsystembackend.repository.ScreenRepository;
 import org.satvik.moviebookingsystembackend.repository.TheatreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +16,6 @@ import java.util.List;
 public class TheatreController {
 
     private final TheatreRepository theatreRepository;
-    private final ScreenRepository screenRepository;
 
     @GetMapping
     public ResponseEntity<List<Theatre>> getAllTheatres() {
@@ -53,22 +50,6 @@ public class TheatreController {
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Theatre> createTheatre(@RequestBody Theatre theatre) {
-
-        Theatre savedTheatre = theatreRepository.save(theatre);
-
-        // Automatically create 5 screens for this theatre
-        for (int i = 1; i <= 5; i++) {
-
-            Screen screen = Screen.builder()
-                    .name("Screen " + i)
-                    .type(Screen.ScreenType.STANDARD)
-                    .totalSeats(120)
-                    .theatre(savedTheatre)
-                    .build();
-
-            screenRepository.save(screen);
-        }
-
-        return ResponseEntity.ok(savedTheatre);
+        return ResponseEntity.ok(theatreRepository.save(theatre));
     }
 }
